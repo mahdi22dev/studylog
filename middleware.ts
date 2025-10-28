c450a7ede99b9080d25b5f46be47c042a0e076f6
mahdi22dev <idrissimahdi2020@gmail.com>
2025-10-28 23:30:26 +0100

    feat: Add '/notes' route to protected routes in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["/timer(.*)", "/forum(.*)"]);
+const isProtectedRoute = createRouteMatcher([
+  "/timer(.*)",
+  "/notes(.*)",
+  "/forum(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
